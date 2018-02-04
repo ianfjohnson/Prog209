82794d76e419c62bcea8da673b041004159a6ca8
ianfjohnson <ianfrederickjohnson@gmail.com>
2018-02-04 12:04:14 -0800

    Updated Exercise-6
    Modified the output from scripts.js to make it look better on index.html

--- Exercise-6/js/scripts.js
+++ Exercise-6/js/scripts.js
@@ -9,19 +9,24 @@ var sunnyInAugust = [false, true, false, true, true];
 
 //planet tiles loop
 for (var i = 0; i < planetNames.length; i++) {
+    //It felt weird having the output just say "false"/"true"
+    var yesno = "Yes";
+    if (sunnyInAugust[i] == false)
+        { yesno = "No"; };
+    
     //class='tile' used for uniformity
     //id='planetName[i]' used for unique stylings
     var nxtPlanet = "<div id='" + planetNames[i] + "' class='tile'>";
     nxtPlanet += "<h1>" + planetNames[i] + "</h1>";
-    nxtPlanet += "<p>" + distancesFromEarth[i] + "</p>";
-    nxtPlanet += "<p>" + sunnyInAugust[i] + "</p>";
+    nxtPlanet += "<p>Distance from Earth: " + distancesFromEarth[i] + "</p>";
+    nxtPlanet += "<p>Sunny In August: " + yesno + "</p>";
     nxtPlanet += "<p>----------</p></div>";
     
     outMsg += nxtPlanet;    
 }
 
 //sunny planets tile loop
-outMsg += "<div id='sunnyPlanets' class='tile'><h1>The following planets are sunny in august:</h1><ul>"
+outMsg += "<div id='sunnyPlanets' class='tile'><h1>The following planets are sunny in August:</h1><ul>"
 for (var i = 0; i < planetNames.length; i++) {
     if (sunnyInAugust[i] == true)
         { outMsg += "<li>" + planetNames[i] + "</li>"}
